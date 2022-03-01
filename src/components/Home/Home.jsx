51e16a9d2c3c535b37a3818378ad6cacf5692ad7
Nguyen Hoang Phi Long <longnhp62@wru.vn>
2022-03-01 20:57:46 +0700

    Post component :+1:

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
+import Post from '../Post/Post'
+
 const Home = () => {
   return (
     <div className='home'>
-      
+      <Post />
     </div>
   )
 }
--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Post = () => {
   return (
     <div className='post'>
-        
+      <div className='post__title'>
+        <div className='post__title__icon'>A</div>
+        <h3>Title</h3>
+      </div>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
     </div>
   )
 }
