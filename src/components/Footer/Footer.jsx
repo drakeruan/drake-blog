f21910345b5507aa1497a147a5e78dba26d25998
longnhp <longnguyenhoangphi@vccorp.vn>
2022-03-24 14:46:32 +0700

    Post, Header style

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
+import './Footer.css'
+
 const Footer = () => {
-  return (
-    <div>Made By Drake Ruan</div>
-  )
+  return <footer id="footer">
+    <div>drakeruan ©️2022</div>
+    <div>
+      <ul>
+        <li><i class='bx bxl-facebook-square'></i></li>
+        <li><i class='bx bxl-linkedin-square'></i></li>
+        <li><i class='bx bxl-github'></i></li>
+        <li><i class='bx bxl-instagram' ></i></li>
+      </ul>
+    </div>
+  </footer>
 }
 
 export default Footer
