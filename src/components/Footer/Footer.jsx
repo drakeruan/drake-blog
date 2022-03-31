d52573e2e6c0da9f52baff057b91319d886ed271
longnhp <longnguyenhoangphi@vccorp.vn>
2022-03-31 17:14:34 +0700

    Footer :+1:

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,14 +5,12 @@ import './Footer.css'
 const Footer = () => {
   return <footer id="footer">
     <div>drakeruan ©️2022</div>
-    <div>
-      <ul>
-        <li><i class='bx bxl-facebook-square'></i></li>
-        <li><i class='bx bxl-linkedin-square'></i></li>
-        <li><i class='bx bxl-github'></i></li>
-        <li><i class='bx bxl-instagram' ></i></li>
-      </ul>
-    </div>
+    <ul class="contact">
+      <li class="contact__item"><i class='bx bxl-facebook-square'></i></li>
+      <li class="contact__item"><i class='bx bxl-linkedin-square'></i></li>
+      <li class="contact__item"><i class='bx bxl-github'></i></li>
+      <li class="contact__item"><i class='bx bxl-instagram' ></i></li>
+    </ul>
   </footer>
 }
 
